c0479e71023bc7dea97b20b5d0da0e892cfa27ad
GiancarloFernandez <giancarlo_fernandez1@usmp.pe>
2021-06-15 17:34:08 -0500

    imagencontroller

---
+++ Controllers/ImagenController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Practica4.Models;
+
+namespace Practica4.Controllers
+{
+    public class ImagenController
+    {
+        {
+        private readonly ImagenContext _context;
+        public ImagenController(ImagenContext context)
+        {
+            _context = context;
+        }
+        [Authorize]
+        public IActionResult Registro(){
+            ViewBag.Comentarios = _context.Comentarios.Select(x => new SelectListItem(x.Nombre, x.Id.ToString()));
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Registro(Imagen i){
+            if (ModelState.IsValid){
+                _context.Imagenes.Add(i);
+                _context.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            } else {
+                return View(i);
+            }
+        }
+    }
+}
