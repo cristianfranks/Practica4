0cb2eb5dc11beb64b266c7ae46c9d5b7047da043
GiancarloFernandez <giancarlo_fernandez1@usmp.pe>
2021-06-15 17:23:42 -0500

    modelo comentario

--- Models/Comentario.cs
+++ Models/Comentario.cs
@@ -1,7 +1,11 @@
+using System.Collections.Generic;
+
 namespace Practica4.Models
 {
-    public class comentario
+    public class Comentario
     {
-        
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public ICollection<Imagen> Imagenes{ get; set; }
     }
 }
